aa2e96fbc1e9ab6501105bcf0fa91fb06fb9fa69
Jeron Aldaron Lau <aldaronlau@gmail.com>
2021-08-29 01:50:41 -0500

    Intialize project

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+// Copyright © 2021 The Lookit Crate Developers
+//
+// Licensed under any of:
+// - Apache License, Version 2.0 (https://www.apache.org/licenses/LICENSE-2.0)
+// - Boost Software License, Version 1.0 (https://www.boost.org/LICENSE_1_0.txt)
+// - MIT License (https://mit-license.org/)
+// At your option (See accompanying files LICENSE_APACHE_2_0.txt,
+// LICENSE_MIT.txt and LICENSE_BOOST_1_0.txt).  This file may not be copied,
+// modified, or distributed except according to those terms.
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
